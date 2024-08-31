44327bad11d49daa5d5030ffb4394ca4d8e43718
SyeddHassan <syeddhassanali@gmail.com>
2024-08-31 09:10:51 -0700

    hero section in process

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,3 +1,5 @@
+import { Button } from "@/components/Button";
+
 import LogoIcon from "@/assets/logo.svg";
 import MenuIon from "@/assets/icon-menu.svg";
 
@@ -31,14 +33,7 @@ export const Header = () => {
           </div>
 
           <div className="flex gap-4 items-center">
-            <button className="py-2 px-3 rounded-lg font-medium text-sm bg-gradient-to-b from-[#190d2e] to-[#4a208a] shadow-[0px_0px_12px_#8c45ff] relative">
-              <div className="absolute inset-0">
-                <div className="rounded-lg border border-white/20 absolute inset-0 [mask-image:linear-gradient(to_bottom,black,transparent)]" />
-                <div className="rounded-lg border border-white/40 absolute inset-0 [mask-image:linear-gradient(to_top,black,transparent)]" />
-                <div className="absolute inset-0 shadow-[0_0_10px_rgba(140, 69, 255, .7)_inset] rounded-lg" />
-              </div>
-              <span>Join Waitlist</span>
-            </button>
+            <Button>Join waitlist</Button>
             <MenuIon className="md:hidden" />
           </div>
         </div>
