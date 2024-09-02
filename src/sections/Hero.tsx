9f81c6b34bdeefdf45b544bce22bf819b2d00131
SyeddHassan <syeddhassanali@gmail.com>
2024-09-02 10:42:00 -0700

    hero section completed along with animations

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,14 +1,34 @@
+"use client";
 /* eslint-disable react/no-unescaped-entities */
+
+import { useRef } from "react";
+import { motion, useScroll, useTransform } from "framer-motion";
 
 import { Button } from "@/components/Button";
 
-import startBg from "@/assets/stars.png";
+import starBg from "@/assets/stars.png";
 
 export const Hero = () => {
+  const sectionRef = useRef(null);
+
+  const { scrollYProgress } = useScroll({
+    target: sectionRef,
+    offset: ["start end", "end start"],
+  });
+
+  const backgroundPositionY = useTransform(
+    scrollYProgress,
+    [0, 1],
+    [-300, 300]
+  );
+
   return (
-    <section
+    <motion.section
+      ref={sectionRef}
       className="h-[492px] md:h-[800px] flex items-center overflow-hidden relative [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_90%,transparent)]"
-      style={{ backgroundImage: `url(${startBg.src})` }}
+      style={{ backgroundImage: `url(${starBg.src})`, backgroundPositionY }}
+      animate={{ backgroundPositionX: starBg.width }}
+      transition={{ repeat: Infinity, duration: 120, ease: "linear" }}
     >
       <div className="absolute inset-0 bg-[radial-gradient(75%_75%_at_center_center,rgb(140,69,255,.5)_15%,rgb(14,0,36,.5)_78%,transparent)]" />
 
@@ -17,24 +37,39 @@ export const Hero = () => {
       {/* End Planet */}
 
       {/* Start Ring 1 */}
-      <div className="absolute w-[344px] h-[344px] md:h-[580px] md:w-[580px] opacity-20 rounded-full border border-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      <motion.div
+        style={{ translateX: "-50%", translateY: "-50%" }}
+        animate={{ rotate: "1turn" }}
+        transition={{ repeat: Infinity, duration: 60, ease: "linear" }}
+        className="absolute w-[344px] h-[344px] md:h-[580px] md:w-[580px] opacity-20 rounded-full border border-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+      >
         <div className="absolute h-2 w-2 bg-white rounded-full top-1/2 left-0 -translate-x-1/2 -translate-y-1/2" />
         <div className="absolute h-2 w-2 bg-white rounded-full top-0 left-1/2 -translate-x-1/2 -translate-y-1/2" />
         <div className="absolute h-5 w-5 border border-white rounded-full top-1/2 left-full -translate-x-1/2 -translate-y-1/2 inline-flex items-center justify-center">
           <div className="h-2 w-2 bg-white rounded-full" />
         </div>
-      </div>
+      </motion.div>
       {/* End Ring 1 */}
 
       {/* Start Ring 2 */}
-      <div className="absolute h-[444px] w-[444px] md:h-[780px] md:w-[780px] rounded-full border border-white/20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 border-dashed" />
+      <motion.div
+        style={{ translateX: "-50%", translateY: "-50%" }}
+        animate={{ rotate: "-1turn" }}
+        transition={{ repeat: Infinity, duration: 60, ease: "linear" }}
+        className="absolute h-[444px] w-[444px] md:h-[780px] md:w-[780px] rounded-full border border-white/20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 border-dashed"
+      />
       {/* End Ring 2 */}
 
       {/* Start Ring 3 */}
-      <div className="absolute h-[544px] w-[544px] md:h-[980px] md:w-[980px] rounded-full opacity-20 border border-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      <motion.div
+        style={{ translateX: "-50%", translateY: "-50%" }}
+        animate={{ rotate: "1turn" }}
+        transition={{ repeat: Infinity, duration: 90, ease: "linear" }}
+        className="absolute h-[544px] w-[544px] md:h-[980px] md:w-[980px] rounded-full opacity-20 border border-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+      >
         <div className="absolute h-2 w-2 bg-white rounded-full top-1/2 left-0 -translate-x-1/2 -translate-y-1/2" />
         <div className="absolute h-2 w-2 bg-white rounded-full top-1/2 left-full -translate-x-1/2 -translate-y-1/2" />
-      </div>
+      </motion.div>
       {/* End Ring 3 */}
 
       <div className="container relative mt-16">
@@ -49,6 +84,6 @@ export const Hero = () => {
           <Button>Join waitlist</Button>
         </div>
       </div>
-    </section>
+    </motion.section>
   );
 };
